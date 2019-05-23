69c68b0e4361cb8b7a4381b4b40d839f7d357ba8
Len Minders <>
2019-05-23 10:53:17 +0200

    add FontAwesome, create profile indicator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ContentPageComponent } from './components/content-page/content-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { ProfileIndicatorComponent } from './components/profile-indicator/profile-indicator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentPageComponent,
     LoginPageComponent,
+    ProfileIndicatorComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    FontAwesomeModule
   ],
   exports: [
     BrowserModule,
