7c3cf1a5147351df8f3f437ef639214cee2afc46
Werner Buys <WBuys@jhb.dvt.co.za>
2019-05-24 14:30:06 +0200

    removed unused import

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
-import { getDefaultService } from 'selenium-webdriver/chrome';
 
 @Injectable({
   providedIn: 'root'
