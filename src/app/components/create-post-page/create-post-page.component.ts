e2b33d3799d703cc21b51d129877286297d7bcdc
Len Minders <>
2019-06-07 10:33:16 +0200

    create page layout

--- src/app/components/create-post-page/create-post-page.component.ts
+++ src/app/components/create-post-page/create-post-page.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-post-page',
   templateUrl: './create-post-page.component.html',
   styleUrls: ['./create-post-page.component.scss']
 })
-export class CreatePostPageComponent implements OnInit {
+export class CreatePostPageComponent {
+  photoSrc: string;
+  // photoSrc = '../../../assets/images/forrest-portrait.jpeg';
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
