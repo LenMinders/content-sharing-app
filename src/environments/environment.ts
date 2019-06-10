ae394d88e76ec09cec013be4a6058b1a8515b9b5
Werner Buys <WBuys@jhb.dvt.co.za>
2019-06-10 14:05:57 +0200

    added environment file

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: 'AIzaSyAhLzbMy4h15ijeWwTfM5CJOLiQqx649pU',
+        authDomain: 'group-project-5ab0b.firebaseapp.com',
+        databaseURL: 'https://group-project-5ab0b.firebaseio.com',
+        projectId: 'group-project-5ab0b',
+        storageBucket: 'group-project-5ab0b.appspot.com',
+        messagingSenderId: '95714405853',
+        appId: '1:95714405853:web:02b387eda3b4b6cb'
+    }
+  };
