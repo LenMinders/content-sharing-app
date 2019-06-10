1e27361b6001f690ff0817245b4d5c63379a827c
Angela Kock <angela@jhb.dvt.co.za>
2019-06-10 10:26:51 +0200

    Code clean up

--- src/app/components/single-image/single-image.component.ts
+++ src/app/components/single-image/single-image.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { User } from 'firebase';
-import { Image } from 'src/app/models/image';
 
 import { faHeart, faComment, faUserCircle } from '@fortawesome/free-solid-svg-icons';
-import { AngularFireDatabase } from '@angular/fire/database/database';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -18,32 +14,16 @@ export class SingleImageComponent implements OnInit {
   faComment = faComment;
   faUserCircle = faUserCircle;
 
-  user: User;
-  images: Image[];
-
   activatedRoute: any;
 
   imageUrl: string;
   displayPhoto: string;
 
-  constructor(private firebaseAuth: AngularFireAuth, public db: AngularFireDatabase, private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.imageUrl = this.route.snapshot.queryParamMap.get('imageUrl');
     this.displayPhoto = this.imageUrl;
-
-    this.firebaseAuth.user.subscribe(
-      user => this.user = user
-    );
-
-    this.firebaseAuth.auth.onAuthStateChanged(user => {
-      this.db.list<Image>(user.uid + '/files')
-        .valueChanges()
-        .subscribe(values => {
-          this.images = values;
-        });
-    });
-
   }
 }
 
