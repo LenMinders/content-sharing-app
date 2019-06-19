c16359167d90fa49549339f5ca4c159f83c1a437
Angela Kock <angela@jhb.dvt.co.za>
2019-06-19 15:16:16 +0200

    Code clean up. Debounce added to search so that requests do not fire at every touch of the keyboard. Fixed add button add button component. Swapped add button and search on mobile view

--- src/app/components/search-page/search-page.component.ts
+++ src/app/components/search-page/search-page.component.ts
@@ -1,14 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from 'firebase';
 import { AngularFireDatabase } from '@angular/fire/database';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { Image } from 'src/app/models/image';
 import { faHeart, faComment, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import {  map } from 'rxjs/operators';
-
-
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-page',
@@ -24,26 +19,27 @@ export class SearchPageComponent implements OnInit {
 
   result: object;
 
+  model: string;
+  modelChanged: Subject<string> = new Subject<string>();
+
   constructor(
     public db: AngularFireDatabase, private http: HttpClient
   ) { }
 
   ngOnInit() {
-    this.http.get('https://us-central1-group-project-5ab0b.cloudfunctions.net/searchPosts?searchTerm=sunflower%20picture')
-    .subscribe((results) => {
-      this.result = results;
-      console.log(results);
-    });
-
+    this.modelChanged.pipe(
+      debounceTime(500),
+      distinctUntilChanged())
+      .subscribe(value => {
+        this.http.get(`https://us-central1-group-project-5ab0b.cloudfunctions.net/searchPosts?searchTerm=${value.replace(' ', '%20')}`)
+          .subscribe((results) => {
+            this.result = results;
+            console.log(results);
+          });
+      });
   }
 
-  onKey(value: string): Observable<Image[]> {
-    return this.http
-      .get<Image>(`https://us-central1-group-project-5ab0b.cloudfunctions.net/searchPosts?searchTerm='${value}')`)
-      .pipe(map(x => (x as any).value));
-
-
+  onKey(value: string) {
+    this.modelChanged.next(value);
   }
 }
-
-
